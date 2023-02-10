d26808496e934bf0e70dfabcfd9651572a49b87a
ZenBakr <99152082+ZenBakr@users.noreply.github.com>
2023-02-10 16:48:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload()
 {
@@ -13,8 +17,27 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " , leftWristY = " + leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rigthWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " , rightWristY = " + rightWristY);
+    }
+}
+function modelLoaded()
+{
+    console.log("PoseNet is initialized");
+}
 function draw()
 {
     image(video,0,0,600,500);
